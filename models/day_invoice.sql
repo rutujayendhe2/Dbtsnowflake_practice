d591bd6224c79172542d99f319cb948e53da1e6d
Rutuja Yendhe <Rutuja.Yendhe@mastek.com>
2025-01-31 08:33:51 +0000

    code

--- models/day_invoice.sql
+++ models/day_invoice.sql
@@ -5,11 +5,11 @@ WITH invoice AS (
         region,
         zone,
         ir.product_id,
-        SUM(quantity) AS total_quantity,  -- Direct sum of quantity
-        SUM(quantity * pm.product_pricing) AS total_value,  -- Calculated: total value based on quantity and product_pricing
-        COUNT(*) AS total_order  -- Direct count of orders
-    FROM SNOWFLAKE_CASE_STUDY.STAGE.invoice_raw ir
-    LEFT JOIN SNOWFLAKE_CASE_STUDY.STAGE.product_master pm 
+        SUM(quantity) AS total_quantity,  
+        SUM(quantity * pm.product_pricing) AS total_value,  
+        COUNT(*) AS total_order 
+    FROM {{ source('stage', 'invoice_raw') }} ir  
+    LEFT JOIN {{ ref('product_master') }} pm  
         ON ir.product_id = pm.product_id
     GROUP BY 1, 2, 3, 4, 5
 ),
@@ -19,15 +19,15 @@ customer AS (
         cust_name,
         cust_location,
         cust_country_code
-    FROM SNOWFLAKE_CASE_STUDY.STAGE.customer_master
+    FROM {{ ref('customer_master') }}  
 ),
 product AS (
     SELECT 
         product_id,
         product_name,
         product_category,
-        product_margin  -- Assuming this represents the margin percentage for the product
-    FROM SNOWFLAKE_CASE_STUDY.STAGE.product_master
+        product_margin
+    FROM {{ ref('product_master') }} 
 )
 SELECT 
     i.transaction_date,
@@ -42,7 +42,6 @@ SELECT
     p.product_category,
     i.total_quantity,
     i.total_value,
-    -- Calculated: total margin = total_value * product_margin / 100
     (i.total_value * p.product_margin) / 100 AS total_margin,  
     i.total_order
 FROM invoice i
--- models/month_invoice.sql
+++ models/month_invoice.sql
@@ -1,4 +1,3 @@
-
 WITH aggregated_invoice AS (
     SELECT
         DATE_TRUNC('MONTH', transaction_date) AS transaction_month, 
@@ -11,11 +10,11 @@ WITH aggregated_invoice AS (
         product_name,
         product_id,
         product_category,
-        SUM(total_quantity) AS total_quantity,  -- Summing total quantities
-        SUM(total_value) AS total_value,  -- Summing total values
-        SUM(total_margin) AS total_margin,  -- Summing total margins
-        SUM(total_order) AS total_order  -- Summing total orders
-    FROM SNOWFLAKE_CASE_STUDY.STAGE.day_invoice  -- Using the existing day_invoice data
+        SUM(total_quantity) AS total_quantity,
+        SUM(total_value) AS total_value,  
+        SUM(total_margin) AS total_margin,  
+        SUM(total_order) AS total_order 
+    FROM {{ ref('day_invoice') }}  
     GROUP BY
         transaction_month,
         customer_country_code,
